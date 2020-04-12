78da8ca9b57ea4f18b0c74eda2e4dd925de6bf2e
Noa Nabeshima <noanabeshima@gmail.com>
2020-04-12 11:52:05 -0700

    added quote from pytorch docs

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ resnet18 = models.resnet18(pretrained=True)
 
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])
+
+'''
+All pre-trained models expect input images normalized in the same way, 
+i.e. mini-batches of 3-channel RGB images of shape (3 x H x W), 
+where H and W are expected to be at least 224. The images have to be loaded in to a range of [0, 1] 
+and then normalized using mean = [0.485, 0.456, 0.406] and std = [0.229, 0.224, 0.225].
+'''
