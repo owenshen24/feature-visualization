bdce21df93dd825ef0d3568d2d7519b869cf8167
Noa Nabeshima <noanabeshima@gmail.com>
2020-04-12 22:38:19 -0700

    minor width changes to conform to PEP8

--- main.py
+++ main.py
@@ -47,7 +47,8 @@ for _ in range(n_iterations):
     for octave in octaves:
         # apply jitter
         y_jitter, x_jitter = np.random.randint(-16, 16, size=2)
-        img = torch.roll(img, shifts=(y_jitter, x_jitter), dims=(-2, -1)).detach()
+        img = torch.roll(img, shifts=(y_jitter, x_jitter), dims=(-2, -1))
+        img = img.detach()
 
         img.requires_grad = True
         logits = octave(img)
@@ -58,7 +59,8 @@ for _ in range(n_iterations):
         g = g/g.abs().mean()
         img = img - learning_rate*g
 
-        # Normalize image https://github.com/eriklindernoren/PyTorch-Deep-Dream
+        # Normalize image 
+        # from https://github.com/eriklindernoren/PyTorch-Deep-Dream
         for c in range(3):
             m, s = mean[c], std[c]
             img[0][c] = torch.clamp(img[0][c], -m/s, (1-m)/s)
